b115d96ee9b3a00f2b1bbf2ebf9c91d7025fcc0d
Rajnish Kumar Jha <96009467+rajnish-jha@users.noreply.github.com>
2021-12-17 19:39:45 +0530

    Pest class is again modified by developer2

--- hello-project/src/hello/com/Pest.java
+++ hello-project/src/hello/com/Pest.java
@@ -9,4 +9,8 @@ class Pest{
   public void m1(){
      System.out.println("This Pest class is added by developer2");
   }
+	
+     public void m3(){
+     System.out.println("This Pest class is modified by developer2 again");
+  }
 }
