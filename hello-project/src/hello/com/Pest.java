741bdaaee3cddf294d089b3fe67b30cdb5e35d93
rajnish-jha <jk.rajnish@gmail.com>
2021-12-17 19:36:35 +0530

    Pest.java class is mdified by developer1 from eclipse

--- hello-project/src/hello/com/Pest.java
+++ hello-project/src/hello/com/Pest.java
@@ -1,6 +1,11 @@
 package hello.com;
 
 class Pest{
+	
+  public void m2(){
+	     System.out.println("This Pest class is added by developer1 also");
+	  }
+
   public void m1(){
      System.out.println("This Pest class is added by developer2");
   }
